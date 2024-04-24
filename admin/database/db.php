8adc19a31c4064b6685d11bf75407f969b555760
Rahil Khatri <php@penguinpeak.com>
2024-04-24 14:39:17 +0530

    Commit from linux machine

--- admin/database/db.php
+++ admin/database/db.php
@@ -1,7 +1,7 @@
 <?php
     define("server" , "localhost");
     define("user" , "root");
-    define("password" , "root");
+    define("password" , "root@123");
     define("database" , "mystudyoffers");
 
     $db = new mysqli(server , user , password , database);
