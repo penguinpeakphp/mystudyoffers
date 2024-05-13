39a2c3af6dad4b307fa96b00d5b61c42dc8ca326
Rahil Khatri <php@penguinpeak.com>
2024-05-13 12:44:42 +0530

    Stop tracking db.php

--- admin/database/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define("server" , "localhost");
-    define("user" , "root");
-    define("password" , "root@123");
-    define("database" , "mystudyoffers");
-
-    $db = new mysqli(server , user , password , database);
-?>
