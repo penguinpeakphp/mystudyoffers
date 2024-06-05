912ae9b31b04c8ee491167124d6912b752432be5
Rahil Khatri <php@penguinpeak.com>
2024-06-05 11:50:13 +0530

    Did minor design changes

--- changepassword.php
+++ changepassword.php
@@ -74,7 +74,6 @@ checksession();
         require_once "partials/footer.php";
         ?>
 
-        <script src="js/bootstrap.min.js"></script>
         <script src="js/custom.js"></script>
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script src="js/changepassword.js"></script>
--- js/custom.js
+++ js/custom.js
@@ -117,16 +117,18 @@ function checklowereducation(md) {
 //collapsible timeline
 
 document.addEventListener('DOMContentLoaded', function () {
-  var coll = document.getElementsByClassName("collapsible");
-  for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
+  if (window.innerWidth < 768) { // Check if the viewport width is less than 768px
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
   }
 });
