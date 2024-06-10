575c3fe35ad77906a166f72080bed0fcc27f23e2
Rahil Khatri <php@penguinpeak.com>
2024-06-10 11:42:15 +0530

    just minor UI change

--- editprofile.php
+++ editprofile.php
@@ -98,7 +98,7 @@ checksession();
                                         <textarea type="text" rows="2" name="editaddress" id="editaddress"></textarea>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-lg-6">
                                     <label for="editgender" class="form-label">Gender</label>
                                     <select id="editgender" name="editgender" class="form-select">
                                         <option selected="" value="" hidden disabled="">Choose Gender...</option> 
@@ -143,25 +143,25 @@ checksession();
                                 <hr />
 
                                 <!-- Parennt Name -->
-                                <div class="col-lg-4">
+                                <div class="col-md-6">
                                     <div class="form-col">
                                         <label for="editparentname">Parent Name</label>
                                         <input type="text" name="editparentname" id="editparentname">
                                     </div>
                                 </div>
-                                <div class="col-lg-4">
+                                <div class="col-md-6">
                                     <div class="form-col">
                                         <label for="editparentemail">Parent Email</label>
                                         <input type="email" name="editparentemail" id="editparentemail">
                                     </div>
                                 </div>
-                                <div class="col-lg-4">
+                                <div class="col-md-6">
                                     <div class="form-col">
                                         <label for="editparentphone">Parent Phone</label>
                                         <input type="text" name="editparentphone" id="editparentphone">
                                     </div>
                                 </div>
-                                <div class="col-lg-6">
+                                <div class="col-md-6">
                                     <div class="form-col">
                                         <label for="editpincode">Pincode</label>
                                         <input type="text" name="editpincode" id="editpincode">
